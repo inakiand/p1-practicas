57f001dafbecc91b8eaf1d56c58f0bef2fa6ce06
inakiand <93219977+inakiand@users.noreply.github.com>
2021-11-10 14:47:10 +0100

    Create Recursividad1.cc

---
+++ Recursividad1.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int mostrar(int );
+
+
+int main(){
+
+    int n , suma;
+
+    cout << " Introduce un numero ";
+    cin >> n;
+    suma = mostrar(n);
+    cout << " = " << suma << endl;
+
+    return 0;
+
+}
+
+int mostrar(int n){
+
+int suma;
+int digito;
+
+suma = 0;
+
+    if (n < 1){
+    
+     cout << " ";}
+ 
+    else {
+    
+       digito = n%10;
+       suma = (digito+mostrar(n/10));
+       cout << " + ";
+       cout << digito << " ";}
+       
+    return suma;
+
+    }
